a3c951e4498b91a2b335df85e6880a027e5b5382
ldocao <ldocao@gmail.com>
2015-03-09 11:53:29 +0900

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygadgetic',
+      version='0.1',
+      description='Generate initial condition for Gadget simulation',
+      url='https://github.com/ldocao/pygadgetic',
+      author='Long DO CAO',
+      author_email='ldocao@gmail.com',
+      license='None',
+      packages=['pygadgetic'],
+      zip_safe=False)
